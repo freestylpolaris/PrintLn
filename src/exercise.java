1391b372d8f9b534f21688728ca7306eb2b725e3
Ryan <Ryan@10.1.9.233>
2013-01-31 04:16:18 -0600

    First commit

---
+++ src/exercise.java
@@ -0,0 +1,21 @@
+/**
+ * This program is a simple demonstration of how to use println on seperate lines
+ * 
+ * &author Ryan Hickman
+ * 
+ */
+public class exercise 
+{
+	public static void main (String[] args)
+	{
+		
+		
+		int sum;										//The sum
+		sum = 249;										//Assigning the sum to a variable of 249
+		System.out.println("The sum of the three " +	//Displays the message on screen
+		"numbers is " + sum);
+		
+		
+	}
+
+}
